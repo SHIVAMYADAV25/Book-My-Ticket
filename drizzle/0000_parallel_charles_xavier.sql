ac7a6c20415a34d1b463393b539d4bad71ae9233
Shivam Yadav <shivamy121314@gmail.com>
2026-04-12 16:44:09 +0530

    user table created

---
+++ drizzle/0000_parallel_charles_xavier.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "user" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50),
+	"is_verified" boolean,
+	"verification_token" varchar(255),
+	"refresh_token" varchar(255),
+	"reset_password_token" varchar(255),
+	"reset_password_expires" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
---
+++ src/db/schema.js
@@ -0,0 +1,22 @@
+import { pgTable, varchar, integer, boolean, timestamp, uuid } from "drizzle-orm/pg-core";
+
+
+export const userTable = pgTable('user', {
+    id: uuid("id").primaryKey().defaultRandom(),
+
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    
+    password: varchar("password", { length: 255 }).notNull(), // 🔥 better than integer
+
+    role: varchar("role", { length: 50 }),
+    isVerified: boolean("is_verified"),
+
+    verificationToken: varchar("verification_token", { length: 255 }),
+    refreshToken: varchar("refresh_token", { length: 255 }),
+    resetPasswordToken: varchar("reset_password_token", { length: 255 }),
+    resetPasswordExpires: timestamp("reset_password_expires"),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date())
+});
