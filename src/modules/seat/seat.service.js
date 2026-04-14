ec7b048b504e9a375a83bd80f981ac281b25378b
Shivam Yadav <shivamy121314@gmail.com>
2026-04-14 14:47:49 +0530

    done with seat services

---
+++ src/modules/seat/seat.service.js
@@ -0,0 +1,31 @@
+import { and, eq } from "drizzle-orm"
+import { db } from "../../common/db"
+import { seatTable } from "../../common/db/schema"
+import ApiError from "../../common/utils/api-error"
+import { getShowById } from "../show/show.service"
+
+const getAvailableSeat = async(showId)=>{
+
+    await getShowById(showId);
+
+    const availableSeat = await db.select().from(seatTable).where(and(eq(seatTable.showId,showId),eq(seatTable.isBooked,false)))
+
+    if(availableSeat.length == 0) throw ApiError.notfound("Seat are not available");
+
+    return availableSeat
+}
+
+const getAllseat = async(showId)=>{
+    await getShowById(showId);
+
+    const allSeat = await db.select().from(seatTable).where(eq(seatTable.showId,showId));
+
+    if(allSeat.length == 0) throw ApiError.notfound("seat are not Available");
+
+    return allSeat
+}
+
+export {
+    getAllseat,
+    getAvailableSeat
+}
