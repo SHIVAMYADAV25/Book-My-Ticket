b5316758e561163b1c0faec8a8c345c498feb2d6
Shivam Yadav <shivamy121314@gmail.com>
2026-04-15 11:50:02 +0530

    done with show router

---
+++ src/modules/show/show.route.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {createShowDto} from "./dto/show.dto.js"
+import * as showController from "./show.controller.js"
+import { authenticate, authorize } from "../auth/auth.middleware.js";
+import validate from "../../common/middleware/validate.middleware.js";
+
+const router = Router();
+
+
+router.get("/movieId/shows",showController.getShowByMovie);
+
+
+// Admin-Only
+
+router.post(
+    "/:movieId/shows",
+    authenticate,
+    authorize("admin"),
+    validate(createShowDto),
+    showController.createShow
+)
+
+export default router;
